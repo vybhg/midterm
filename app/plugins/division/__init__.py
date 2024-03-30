8526355038922894bd8fde3708d59d209ab03536
vybhg <158224420+vybhg@users.noreply.github.com>
2024-03-30 13:20:52 -0400

    Update __init__.py

--- app/plugins/division/__init__.py
+++ app/plugins/division/__init__.py
@@ -3,15 +3,11 @@ from app.commands import Command
 class DivisionCommand(Command):
     def execute(self, args):
         if args:
+            a = float(args[0])
+            b = float(args[1])
             try:
-                a = float(args[0])
-                b = float(args[1])
-                if b != 0:
-                    result = a / b
-                    return result
-                else:
-                    print("Error: Division by zero is not allowed")
-            except (ValueError, ZeroDivisionError, IndexError):
-                print("Error: Please provide two valid numbers as arguments.")
+                return a/b
+            except:
+                print ("division by zero error ")
         else:
-            print("Error: No numbers provided for division")
+            print ("nothing to Divide")
