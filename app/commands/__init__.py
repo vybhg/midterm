d82f4b7b5a8efebe8807ff0aca123e63d3aeb8ec
vybhg <158224420+vybhg@users.noreply.github.com>
2024-03-30 01:19:05 -0400

    Update __init__.py

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -12,14 +12,17 @@ class CommandHandler:
     def __init__(self):
         self.commands = {}
 
-    def register_command(self, command_name: str, command_class):
+    def register_command(self, command_name, command_class):
         self.commands[command_name] = command_class
 
-    def execute_command(self, command_name: str, *args):
+    def execute_command(self, command_name, *args):
         try:
-            command_class = self.commands[command_name]
-            command_instance = command_class()
-            command_instance.execute(args)
-        except KeyError:
-            raise NoSuchCommandError(f"No such command: {command_name}")
+            command_class = self.commands.get(command_name)
+            if command_class:
+                command_instance = command_class()
+                command_instance.execute(args)
+            else:
+                raise NoSuchCommandError(f"No such command: {command_name}")
+        except NoSuchCommandError as e:
+            print(e)
 
