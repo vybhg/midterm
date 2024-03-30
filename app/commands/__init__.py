37630b829c649dddb95ab6a8b5273760f71385be
vybhg <158224420+vybhg@users.noreply.github.com>
2024-03-30 12:57:41 -0400

    Update __init__.py

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -5,23 +5,24 @@ class Command(ABC):
     def execute(self):
         pass
 
-class CommandNotFoundError(Exception):
+class NoSuchCommandError(Exception):
     pass
 
 class CommandHandler:
     def __init__(self):
-        self.commands_mapping = {}
+        self.commands = {}
 
     def register_command(self, command_name: str, command_class):
-        self.commands_mapping[command_name] = command_class
+        self.commands[command_name] = command_class
 
     def execute_command(self, command_name: str, *args):
         try:
-            command_class = self.commands_mapping[command_name]
+            command_class = self.commands[command_name]
             command_instance = command_class()
             command_instance.execute(*args)
         except KeyError:
-            raise CommandNotFoundError(f"No such command: {command_name}")
+            raise NoSuchCommandError(f"No such command: {command_name}")
+
 
 
 
